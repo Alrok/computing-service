1012c26417794a7b3c4512969c1564dfde86d895
Oleksandr Rokytskyi <1rokytskyy@gmail.com>
2019-10-26 21:41:11 +0300

    Implement sorting for current user

--- resources/product.py
+++ resources/product.py
@@ -1,5 +1,7 @@
+import json
 from flask_restful import Resource, reqparse
 
+from es.indices.analytics import Analytics
 from es.indices.products import Products
 
 
@@ -11,9 +13,24 @@ class Product(Resource):
         parser.add_argument('userId', type=int)
         parameters = parser.parse_args()
 
-        if parameters['userId']:
-            return {'message': 'TODO: products for user should be calculated'}, 500
+        request = json.loads(parameters['request'])
 
-        items = Products.instance().search(parameters['request'])
+        if parameters['userId']:
+            user_analytics = Analytics.instance().search({'query': {'term': {'userId': parameters['userId']}}})
+
+            product_sort = dict()
+            for event in user_analytics:
+                if event['event']['productId'] not in product_sort.keys():
+                    product_sort[event['event']['productId']] = 0
+
+                product_sort[event['event']['productId']] += 1
+
+            request['sort'] = [{'_script': {'order': 'desc', 'type': 'number', "script": {
+                'lang': 'painless',
+                "source": "double factor = 1.0E-5;try {factor = params.product_sort[doc['_id'].value];} catch (NullPointerException e) {factor =1.0E-5;} return factor;",
+                'params': {"product_sort": product_sort}
+            }}}]
+
+        items = Products.instance().search(json.dumps(request))
 
         return {'items': items}
